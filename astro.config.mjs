e369b013bb339cdeaca6e9c8b0c8d560ee8bdd0d
Alexis LOURS <alexislours@protonmail.com>
2025-12-06 13:10:42 +0100

    feat: use astro font loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexislours.com",
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Newsreader",
+        cssVariable: "--font-newsreader",
+        weights: [300, 400, 500, 600],
+        styles: ["normal", "italic"],
+        fallbacks: ["serif"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Inter",
+        cssVariable: "--font-inter",
+        weights: [400, 500, 600],
+        styles: ["normal"],
+        fallbacks: ["system-ui", "-apple-system", "sans-serif"],
+      }
+    ]
+  },
 
   vite: {
     plugins: [tailwindcss()]
